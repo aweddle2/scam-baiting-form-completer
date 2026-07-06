db7348155e07f23425827309d432a903572f95b3
Andrew Weddle <aweddle@gmail.com>
2026-07-06 21:36:11 +1000

    added note

--- src/Program.cs
+++ src/Program.cs
@@ -2,7 +2,7 @@ var urls = new[]
 {
     //"https://form.jotform.com/261687136210050",
     "https://forms.office.com/pages/responsepage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAZAAA3614JUMDlUUEZRRjU2TEdCMVpOSVBIN1UyRjNTTC4u&origin=lprLink&route=shorturl",
-    "https://forms.fillout.com/t/9GAFdjS6fkus",
+    //"https://forms.fillout.com/t/9GAFdjS6fkus",  This has been deleted, but useful for when looking at automating a skip if it has been deleted.
 };
 
 int runCount = args.Length > 0 && int.TryParse(args[0], out var parsedCount) ? parsedCount : 10;
